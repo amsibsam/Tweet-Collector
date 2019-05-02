9d7b17663fe6e8254f532ccd7f84e12e9e78d5f9
Rahardyan Bisma <rahardyanbismasetya.putra@mitrais.com>
2019-05-02 09:59:53 +0700

    Initial Commit

---
+++ Tweet Scrapper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tweet Scrapper
+//
+//  Created by Rahardyan Bisma Setya Putra on 02/05/19.
+//  Copyright © 2019 Rahardyan Bisma Setya Putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
